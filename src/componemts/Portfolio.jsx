6f64d265815a03d7b23930d66a96a6402d46c040
quadri090 <99067374+quadri090@users.noreply.github.com>
2024-01-14 02:11:57 +0100

    update: portfolio

--- src/componemts/Portfolio.jsx
+++ src/componemts/Portfolio.jsx
@@ -2,21 +2,25 @@ import React, { useRef, useState } from "react";
 import PortfolioCard from "./PortfolioCard";
 import chow from "../assets/chow.png";
 import fintekk from "../assets/fintekk.png";
-import construction from "../assets/construction.png"
-import smtp from "../assets/smtp-app.png"
+import construction from "../assets/construction.png";
+import smtp from "../assets/smtp-app.png";
+import shopbuddy from "../assets/shopbuddy.png";
 import Headings from "./Headings";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
 export default function Portfolio() {
   return (
-    <div className="py-[100px] lg:mt-[200px]" id='portfolio' aria-labelledby="portfolio">
+    <div
+      className="py-[100px] lg:mt-[200px]"
+      id="portfolio"
+      aria-labelledby="portfolio"
+    >
       <Headings header="Portfolio" desc="My past projects" />
       <div className=" rounded-xl shadow-2xl ">
-
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
@@ -33,33 +37,42 @@ export default function Portfolio() {
         >
           <SwiperSlide>
             <PortfolioCard
+              src={shopbuddy}
+              head="ShopBuddy"
+              desc="An ecommerce app built on TailwindCSS, ReactJS, NextJS, Prisma & Mongodb Responsive on all devivces."
+              hrefff="https://chowchow-three.vercel.app/"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PortfolioCard
               src={chow}
-              head="Food Ordering App"
-              desc="Responsive modern components with adaptive UI components for chowShop. Responsive on all devivces."
-              hrefff='https://chowchow-three.vercel.app/'
+              head="ChowShop"
+              desc="A food ordering app utilizing TailwindCSS, ReactJS, NextJS, Mongoose & MongoDB."
+              hrefff="https://chowchow-three.vercel.app/"
             />
           </SwiperSlide>
           <SwiperSlide>
             <PortfolioCard
               src={smtp}
-              head="Email server"
-              desc="An Email client utilizing nodejs and gmail SMTP"
-              hrefff='https://smtp-app.vercel.app/'
+              head="Email Server"
+              desc="An Email client built with React & Nextjs, NodeMailer & gmail SMTP."
+              hrefff="https://smtp-app.vercel.app/"
             />
           </SwiperSlide>
           <SwiperSlide>
             <PortfolioCard
               src={construction}
-              head="Construction website"
-              desc="Responsive modern components with adaptive UI components for Eco-builders. Responsive on all devivces."
-              hrefff='https://construction-landing-page-seven.vercel.app/'
+              head="Eco-Builders"
+              desc="Construction website landing page. Responsive on all devivces."
+              hrefff="https://construction-landing-page-seven.vercel.app/"
             />
           </SwiperSlide>
           <SwiperSlide>
-            <PortfolioCard src={fintekk} 
-              head="Fintech Landing Page"
-              desc="Responsive modern components with adaptive UI components for Fintekk. Responsive accross multiple devivces."
-              hrefff='https://fintekk-project.vercel.app/'
+            <PortfolioCard
+              src={fintekk}
+              head="Fintech"
+              desc="Fintech company landing page. Responsive accross multiple devivces."
+              hrefff="https://fintekk-project.vercel.app/"
             />
           </SwiperSlide>
         </Swiper>
